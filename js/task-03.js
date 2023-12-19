9f7c2fa8f33497d7cc4e00f61cba11b686238736
YuriiRogovets <yura.rogovets@gmail.com>
2023-12-19 12:06:11 +0200

    create hw-08

--- js/task-03.js
+++
@@ -1,9 +0,0 @@
-const nameInput = document.querySelector("#name-input")
-const nameOutput = document.querySelector("#name-output")
-
-nameInput.addEventListener("input", (event) => {
-  nameOutput.textContent = event.currentTarget.value.trim();
-  if (nameOutput.textContent === ``) {
-    nameOutput.textContent = `Anonymous`;
-  }
-});
